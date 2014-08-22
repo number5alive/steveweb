0bf24516e1e3c42363e6ab72bae00b17ee1a3c27
Steve <number5alive@gmail.com>
2014-08-22 14:38:03 -0400

    created a main app... just because

--- steveweb/steveweb/settings.py
+++ steveweb/steveweb/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +81,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+TEMPLATE_URL = '/main/templates/'
--- steveweb/steveweb/urls.py
+++ steveweb/steveweb/urls.py
@@ -5,8 +5,20 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'steveweb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'main.views.home', name='main_home'),
+)
+
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+
+    url(r'^login/$', 'login',
+        {'template_name': 'login.html'},
+        name = 'steveweb_login'),
+
+    url(r'^logout/$', 'logout',
+        {'next_page': 'steveweb_home.html'},
+        name = 'steveweb_logout'),
 )
