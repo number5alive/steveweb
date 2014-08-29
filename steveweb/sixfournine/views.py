a9374dcef49d5e68eaf9932776e555aefe55466e
Steve <number5alive@gmail.com>
2014-08-28 22:46:21 -0400

    made a carousel for the sixfournine page

--- steveweb/sixfournine/views.py
+++ steveweb/sixfournine/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from sixfournine.models import Ticket
+
 # Create your views here.
 def home(request):
-    context = { } 
+    all_tickets = Ticket.objects.all()
+    context = { 'all_tickets': all_tickets } 
     return render(request, "sixfournine/home.html", context)
 
